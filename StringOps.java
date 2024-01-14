83db3a971bfc1b319126ec9d69f33015ffbc864f
amitfrumkin <amit.frumkin@post.runi.ac.il>
2024-01-14 21:52:18 +0200

    vowels

--- StringOps.java
+++ StringOps.java
@@ -22,13 +22,35 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+      String string = "vOwEls ArE fUn"; 
+       System.out.println(capVowelsLowRest(string)) ; 
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
-    }
+
+        String newStr = "" ;
+        char [] vowels = {'a','e','i','o','u'} ;
+         
+        for(int i = 0 ; i < string.length() ; i++){
+            
+            for(int j = 0 ; j < vowels.length ; j++ ){
+
+                if(string.charAt(i) == vowels[j]){
+
+                    newStr = newStr + (char) (string.charAt(i) - 32) ;
+                }
+            }
+
+        if(string.charAt(i) > 65 && string.charAt(i) <= 91 && string.charAt(i) != 69 && string.charAt(i) != 73 && string.charAt(i) != 79 && string.charAt(i) != 85 ){
+
+            newStr = newStr + (char) (string.charAt(i) + 32) ;
+        }else if (string.charAt(i) != 97 && string.charAt(i) != 101 && string.charAt(i) != 105 && string.charAt(i) != 111 && string.charAt(i) != 107){
+            newStr = newStr + string.charAt(i) ;
+        }
+        }
+
+        return newStr;
+    } 
 
     public static String camelCase (String string) {
         // Write your code here:
